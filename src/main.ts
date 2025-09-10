037e05efa73fa9fa1dd843e2a2121bef36f92cf0
Oleksandr Baranov <sashok1987nk@gmail.com>
2025-09-10 11:02:15 +0200

    update task-7.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@
 // import "./task-3.ts";
 // import "./task-4.ts";
 // import "./task-5.ts";
-import "./task-6.ts";
-// import "./task-7.ts";
+// import "./task-6.ts";
+import "./task-7.ts";
 // import "./task-8.ts";
